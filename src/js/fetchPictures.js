521c787481c13920d271053dbfc19d0681c62efe
Alex <afanasbev1@gmail.com>
2023-04-10 15:40:48 +0300

    initialcommit: realisation fetchPictures

---
+++ src/js/fetchPictures.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+export class ImageApiService {
+  #API_KEY = '35263263-87e16adcb3e852982f83f3522';
+  BASE_URL = 'https://pixabay.com/';
+
+  searchQuery = '';
+  pageNumber = 1;
+
+  searchParams = new URLSearchParams({
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 12,
+    lang: 'ru',
+  });
+
+
+  async fetchPictures() {
+    const response = await axios.get(
+      `${this.#BASE_URL}api/?key=${this.#API_KEY}&q=${this.searchQuery}&${
+        this.searchParams
+      }&page=${this.pageNumber}`
+    );
+    return response.data;
+  }
+
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  }
+
+  get pageNumber() {
+    return this.pageNumber;
+  }
+
+  incrementPage() {
+    this.pageNumber += 1;
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+}
